fec176d9a516dfe6b9685021e486f5e2bb27c197
evancurls <156024750+evancurls@users.noreply.github.com>
2024-09-05 22:00:30 -0700

    Add files via upload

---
+++ auto-photo.py
@@ -0,0 +1,31 @@
+import pytesseract
+import cv2
+import PIL.Image
+# copy paste directory to the images
+# C:\Users\inrig\OneDrive\Documents\Python Text Files\OpenCV\pugs
+
+#default configuration for the image processing: use 1
+myconfig = r"--psm 1 --oem 3"
+#6 is good for kda
+
+#what file will be processed and turned into text
+myIMG = "C:/Users/inrig/OneDrive/Documents/Python Text Files/OpenCV/pugs/game3names.PNG"
+text = pytesseract.image_to_string(PIL.Image.open(myIMG), config=myconfig)
+print(text)
+
+list = text.split()
+num = 0;
+ssh = 0;
+cs = 0;
+for x in list:
+    if(x == "super"):
+        list[num] = list[num] + "." + list[num + 1] + "." + list[num + 2]
+        list.pop(num + 1)
+        list.pop(num + 1)
+    if(x == "Chad"):
+        list[num] = list[num] + " " + list[num + 1]
+        list.pop(num + 1)
+    num = num + 1
+    print(x)
+ 
+print(list)
